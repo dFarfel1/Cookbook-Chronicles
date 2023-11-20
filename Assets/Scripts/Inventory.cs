e727a278a08b7d90a09e820cf306a145c30a80c0
Daniel Farfel <96895199+dFarfel1@users.noreply.github.com>
2023-11-19 21:11:35 -0500

    Change to 24 inventory items and arrow keys for selecting

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+	private static int numItems = 24;
+	private static int numRows = 4;
+	private static int numCols = 6;
+	private int selectedIndex;
+	private int selectedRow;
+	private int selectedCol;
+	private GameObject[] items;
+	public GameObject[] itemsDisplay;
+	// Start is called before the first frame update
+	void Start()
+	{
+		selectedIndex = 0;
+		selectedRow = 0;
+		selectedCol = 0;
+		items = new GameObject[numItems];
+		itemsDisplay = new GameObject[numItems];
+
+		for (int i = 1; i <= numItems; i++)
+		{
+			itemsDisplay[i - 1] = GameObject.Find("item" + i.ToString());
+		}
+
+
+		/*Debug.Log(itemsDisplay.Length);
+		Debug.Log(itemsDisplay[6]);*/
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			selectedCol = (selectedCol - 1 + numCols) % numCols;
+			select();
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			selectedCol = (selectedCol + 1) % numCols;
+			select();
+		}
+		else if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			selectedRow = (selectedRow - 1 + numRows) % numRows;
+			select();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			selectedRow = (selectedRow + 1) % numRows;
+			select();
+		}
+
+		if (Input.GetKey("l"))
+		{
+			drop();
+		}
+	}
+
+	private void select()
+	{
+		int index = selectedRow * numCols + selectedCol;
+
+		/*Debug.Log("Row: " + selectedRow.ToString() + "Col: " + selectedCol.ToString());
+		Debug.Log("New: " + index.ToString() + " Old: " + selectedIndex.ToString());*/
+
+		itemsDisplay[selectedIndex].GetComponent<Image>().color = Color.white;
+
+		selectedIndex = index;
+		itemsDisplay[selectedIndex].GetComponent<Image>().color = Color.green;
+	}
+
+	void OnEnterItemTrigger(GameObject item)
+	{
+		if (Input.GetKey("p"))
+		{
+			pickup(item);
+		}
+	}
+
+	private void drop()
+	{
+		if (selectedIndex >= 0 && items[selectedIndex] != null)
+		{
+			items[selectedIndex].transform.position = gameObject.transform.position;
+			items[selectedIndex].SetActive(true);
+			itemsDisplay[selectedIndex].GetComponent<Text>().text = "none";
+			items[selectedIndex] = null;
+		}
+	}
+
+	private void pickup(GameObject item)
+	{
+		for (int i = 0; i < numItems; i++)
+		{
+			if (items[i] == null)
+			{
+				items[i] = item;
+				itemsDisplay[i].GetComponent<Text>().text = item.name;
+				item.SetActive(false);
+			}
+		}
+	}
+
+}
