57fd58736fc06536a55a2df5f9d5c5acdcf45a88
Daniel Farfel <96895199+dFarfel1@users.noreply.github.com>
2023-11-20 18:42:45 -0500

    Make generated items grow

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
 	public int count;
-	private static int max = 50;
+	public static int max = 50;
 
 	public GameObject prefab;
 	public float radius;
+	public float growthFactor;
 
 	void Start()
 	{
 		count = 0;
-		radius = 5.0f;
+		radius = 50.0f;
+		growthFactor = 1.05f;
 
 		StartCoroutine("spawn");
+		StartCoroutine("grow");
 	}
 
 
@@ -32,10 +35,25 @@ public class NewBehaviourScript : MonoBehaviour
 			{
 				count++;
 				Vector3 position = new Vector3(transform.position.x + Random.Range(-radius, radius), transform.position.y, transform.position.z + Random.Range(-radius, radius));
-				GameObject.Instantiate(prefab, position, Quaternion.identity).SetActive(true);
+				GameObject newObject = GameObject.Instantiate(prefab, position, Quaternion.identity);
+				newObject.SetActive(true);
+
+				newObject.transform.SetParent(gameObject.transform);
 			}
 
 			yield return new WaitForSeconds(1.5f);
+		}
+	}
+
+	IEnumerator grow()
+	{
+		while (true)
+		{
+			foreach(Transform item in transform)
+			{
+				item.localScale *= growthFactor;
+			}
+			yield return new WaitForSeconds(6.0f);
 		}
 	}
 }
