ace9ff31e09d453d478673a06fa1bc4005a52e85
Daniel Farfel <96895199+dFarfel1@users.noreply.github.com>
2023-11-20 00:22:22 -0500

    Make items stackable

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -11,20 +11,25 @@ public class Inventory : MonoBehaviour
 	private int selectedIndex;
 	private int selectedRow;
 	private int selectedCol;
-	private GameObject[] items;
+	private inventoryItem[] items;
 	public GameObject[] itemsDisplay;
+
+	private bool open;
 	// Start is called before the first frame update
 	void Start()
 	{
 		selectedIndex = 0;
 		selectedRow = 0;
 		selectedCol = 0;
-		items = new GameObject[numItems];
+		items = new inventoryItem[numItems];
 		itemsDisplay = new GameObject[numItems];
+
+		open = false;
 
 		for (int i = 1; i <= numItems; i++)
 		{
 			itemsDisplay[i - 1] = GameObject.Find("item" + i.ToString());
+			items[i - 1] = new inventoryItem();
 		}
 
 
@@ -36,8 +41,21 @@ public class Inventory : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if (Input.GetKeyDown("b"))
+		{
+			if (open)
+			{
+				GetComponent<Canvas>().enabled = false;
+				open = false;
+			}
+			else
+			{
+				GetComponent<Canvas>().enabled = true;
+				open = true;
+			}
+		}
 
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && open)
 		{
 			selectedCol = (selectedCol - 1 + numCols) % numCols;
 			select();
@@ -57,11 +75,12 @@ public class Inventory : MonoBehaviour
 			selectedRow = (selectedRow + 1) % numRows;
 			select();
 		}
-
-		if (Input.GetKey("l"))
+		else if (Input.GetKeyDown("l"))
 		{
 			drop();
 		}
+
+
 
 		//Temporary adding items
 		if (Input.GetKeyDown("p"))
@@ -94,27 +113,98 @@ public class Inventory : MonoBehaviour
 
 	private void drop()
 	{
-		if (selectedIndex >= 0 && items[selectedIndex] != null)
+		if (selectedIndex >= 0 && items[selectedIndex].getName() != "None")
 		{
-			items[selectedIndex].SetActive(true);
-			items[selectedIndex].transform.position = GameObject.Find("ModularCharacterPBR").transform.position;
-			itemsDisplay[selectedIndex].GetComponentInChildren<Text>().text = "none";
-			items[selectedIndex] = null;
+			items[selectedIndex].generateClone(GameObject.Find("ModularCharacterPBR").transform.position);
+			int numItems = items[selectedIndex].drop();
+
+			if (numItems == 0)
+			{
+				itemsDisplay[selectedIndex].GetComponentInChildren<Text>().text = "none";
+			}
+
+			itemsDisplay[selectedIndex].GetComponentsInChildren<Text>()[1].text = "Count: " + numItems.ToString();
+			
 		}
 	}
 
 	private void pickup(GameObject item)
 	{
+		bool found = false;
 		for (int i = 0; i < numItems; i++)
 		{
-			if (items[i] == null)
+			if (items[i].getName() == item.name)
 			{
-				items[i] = item;
-				itemsDisplay[i].GetComponentInChildren<Text>().text = item.name;
+				found = true;
+				itemsDisplay[i].GetComponentsInChildren<Text>()[1].text = "Count: " + items[i].add().ToString();
 				item.SetActive(false);
 				break;
 			}
 		}
+
+		if (!found)
+		{
+			for (int i = 0; i < numItems; i++)
+			{
+				if (items[i].getName() == "None")
+				{
+					items[i].addFirstItem(item);
+					itemsDisplay[i].GetComponentInChildren<Text>().text = item.name;
+					itemsDisplay[i].GetComponentsInChildren<Text>()[1].text = "Count: ";
+					item.SetActive(false);
+					break;
+				}
+			}
+		}
 	}
 
+}
+
+class inventoryItem
+{
+	private int count;
+	private GameObject gameObject;
+
+	public inventoryItem()
+	{
+		
+	}
+
+	public int add()
+	{
+		count++;
+		return count;
+	}
+
+	public void addFirstItem(GameObject gameObject)
+	{
+		this.gameObject = gameObject;
+		count = 1;
+	}
+
+	public int drop()
+	{
+		if (count == 1)
+		{
+			gameObject = null;
+		}
+
+		count--;
+		return count;
+	}
+
+	public void generateClone(Vector3 position)
+	{
+		GameObject.Instantiate(gameObject, position, Quaternion.identity).SetActive(true);
+	}
+
+	public string getName()
+	{
+		if (gameObject == null)
+		{
+			return "None";
+		}
+
+		return gameObject.name;
+	}
 }
