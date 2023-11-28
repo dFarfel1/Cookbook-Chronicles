d45ce83eb949c3f408919b14176e3f198bf0b74d
jinc0930 <jinc0930@gmail.com>
2023-11-28 18:37:35 -0500

    Testing out Disable MouseDirection of camera
    NOT WORKING

--- Assets/Scripts/pauseMenu.cs
+++ Assets/Scripts/pauseMenu.cs
@@ -7,6 +7,11 @@ public class pauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject resumeMenu;
 
+
+    public bool isGamePaused(){
+        return GameIsPaused;
+    }
+    
     // Update is called once per frame
     void Update()
     {
@@ -18,17 +23,19 @@ public class pauseMenu : MonoBehaviour
                 Pause();
             }
         }
-        
     }
 
     void Resume(){
         resumeMenu.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+       
     }
     void Pause(){
         resumeMenu.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
