88de0b0effc1aba07bfa4e52c2c04fd2beb3d7a2
Daniel Farfel <96895199+dFarfel1@users.noreply.github.com>
2023-12-15 19:39:16 -0500

    label UI

--- Assets/Scripts/I_Item.cs
+++ Assets/Scripts/I_Item.cs
@@ -3,4 +3,15 @@ using UnityEngine;
 public interface I_Item
 {
 	public Sprite getSprite();
+
+	public int getProtien();
+
+	public int getCarbs();
+
+	public int getFiber();
+
+	public int getCalories();
+
+	public bool isPoison();
+
 }
